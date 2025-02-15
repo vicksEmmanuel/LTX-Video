dfa9be0448b7936d4e9a02406b4891516baf4d53
Yoav HaCohen <yoavhacohen@gmail.com>
2025-02-15 22:51:36 +0200

    Tests: Add some tests

---
+++ tests/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+import shutil
+import os
+
+
+@pytest.fixture(scope="session")
+def test_paths(request, pytestconfig):
+    try:
+        output_path = "output"
+        ckpt_path = request.param  # This will get the current parameterized item
+        text_encoder_model_name_or_path = pytestconfig.getoption(
+            "text_encoder_model_name_or_path"
+        )
+        input_image_path = pytestconfig.getoption("input_image_path")
+
+        config = {
+            "ckpt_path": ckpt_path,
+            "input_image_path": input_image_path,
+            "output_path": output_path,
+            "text_encoder_model_name_or_path": text_encoder_model_name_or_path,
+        }
+
+        yield config
+
+    finally:
+        if os.path.exists(output_path):
+            shutil.rmtree(output_path)
+
+
+def pytest_generate_tests(metafunc):
+    if "test_paths" in metafunc.fixturenames:
+        ckpt_paths = metafunc.config.getoption("ckpt_path")
+        metafunc.parametrize("test_paths", ckpt_paths, indirect=True)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--ckpt_path",
+        action="append",
+        default=[],
+        help="Path to checkpoint files (can specify multiple)",
+    )
+    parser.addoption(
+        "--text_encoder_model_name_or_path",
+        action="store",
+        default="PixArt-alpha/PixArt-XL-2-1024-MS",
+        help="Path to the checkpoint file",
+    )
+    parser.addoption(
+        "--input_image_path",
+        action="store",
+        default="tests/utils/car.png",
+        help="Path to input image file.",
+    )
